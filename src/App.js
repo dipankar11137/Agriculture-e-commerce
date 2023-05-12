af613a18ee93b8cf224777df4bf95c1ed29d4bfd
Dipankar <dipankar15-11137@diu.edu.bd>
2023-05-12 13:16:28 +0600

    add manage item initial

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import Blog from "./Components/Pages/Blog/Blog";
 import BuyProducts from "./Components/Pages/BuyProducts/BuyProducts";
+import ManageItems from "./Components/Pages/ManageItem.js/ManageItems";
 AOS.init();
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/blog" element={<Blog />}></Route>
         <Route path="/buy/:id" element={<BuyProducts />}></Route>
+        <Route path="/manageItem" element={<ManageItems />}></Route>
         <Route path="/createAccount" element={<CreateAccount />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/*" element={<NotFound />}></Route>
