a9f56a276a242e95349f5515adbac9aae44458a9
Dipankar <dipankar15-11137@diu.edu.bd>
2023-05-13 13:01:47 +0600

    manage item

--- src/Components/Pages/ManageItem.js/ManageItems.js
+++ src/Components/Pages/ManageItem.js/ManageItems.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ManageItem from "./ManageItem";
 
 const ManageItems = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, [products]);
   return (
     <div>
-      <ManageItem />
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product, index) => (
+              <ManageItem
+                key={product._id}
+                product={product}
+                index={index + 1}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
