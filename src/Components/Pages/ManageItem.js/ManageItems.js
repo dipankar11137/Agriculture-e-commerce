f283c4f90415027ef95a6753b991f16e21721113
Dipankar <dipankar15-11137@diu.edu.bd>
2023-05-13 13:25:09 +0600

    manage item add modal

--- src/Components/Pages/ManageItem.js/ManageItems.js
+++ src/Components/Pages/ManageItem.js/ManageItems.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import ManageItem from "./ManageItem";
-
+import { FaUpload } from "react-icons/fa";
+import { FaHandPointRight } from "react-icons/fa";
+import ManageAddItem from "./ManageAddItem";
 const ManageItems = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
@@ -10,6 +12,26 @@ const ManageItems = () => {
   }, [products]);
   return (
     <div>
+      {/* modal start */}
+      <div className="flex justify-center my-3">
+        <h1
+          data-aos="fade-right"
+          data-aos-duration="3000"
+          className="mt-1 text-4xl uppercase font-extrabold flex text-accent"
+        >
+          Add A Product <FaHandPointRight className="ml-4 mt-1 text-primary" />
+        </h1>
+        <label
+          htmlFor="sellingModal"
+          className="btn btn-primary text-xl text-white ml-20 font-bold "
+        >
+          Click Here{" "}
+          <FaUpload className="text-white ml-5 text-3xl animate-bounce " />
+        </label>
+        <ManageAddItem />
+      </div>
+
+      {/* modal end */}
       <div className="overflow-x-auto w-full">
         <table className="table w-full">
           {/* head */}
