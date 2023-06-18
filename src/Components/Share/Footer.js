2b8073d3d82bf72ce83efced3143f71e5b947449
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-06-18 12:42:26 +0600

    edit somethings

--- src/Components/Share/Footer.js
+++ src/Components/Share/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-3 bg-accent  text-white  rounded">
+    <footer className="footer footer-center p-3 bg-accent  text-white  ">
       <div>
         <p>Copyright © 2023 </p>
         <p>Develop By Mijanur</p>
